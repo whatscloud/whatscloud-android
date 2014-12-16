cf4552e1ad99c9653d617237c5dbc505b03fc0d6
WhatsCloud <whatscloudapp@gmail.com>
2014-12-15 22:15:46 -0500

    Improve WhatsApp kill command compatibility

--- src/com/whatscloud/config/integration/WhatsAppInterface.java
+++ src/com/whatscloud/config/integration/WhatsAppInterface.java
@@ -8,6 +8,6 @@ public class WhatsAppInterface
     public static String MESSAGE_DB = "/data/data/" + PACKAGE + "/databases/msgstore.db";
     public static String CONTACTS_DB = "/data/data/" + PACKAGE + "/databases/wa.db";
     public static String PATH_TO_BUSYBOX_BINARY = "/data/data/" + WhatsCloud.PACKAGE + "/files/busybox.bin";
-    public static String STOP_WHATSAPP_COMMAND = PATH_TO_BUSYBOX_BINARY + " kill -9 $( ps | grep " + PACKAGE + " | " + PATH_TO_BUSYBOX_BINARY + " awk '{ print $2 }')";
+    public static String STOP_WHATSAPP_COMMAND = PATH_TO_BUSYBOX_BINARY + " killall " + PACKAGE;
     public static String START_MESSAGING_SERVICE_COMMAND = "am startservice -n com.whatsapp/com.whatsapp.messaging.MessageService -a com.whatsapp.MessageService.START";
 }
