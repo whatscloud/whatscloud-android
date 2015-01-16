615ef6a9bdc5054508f1818a4b1a0ce9190ef7d5
WhatsCloud <whatscloudapp@gmail.com>
2015-01-16 19:28:08 +0200

    v1.2.0
    Increase max contacts per sync

--- src/com/whatscloud/config/functionality/Sync.java
+++ src/com/whatscloud/config/functionality/Sync.java
@@ -3,7 +3,7 @@ package com.whatscloud.config.functionality;
 public class Sync
 {
     public static int DEFAULT_INTERVAL = 5000;
-    public static int MAX_CHATS_PER_SYNC = 20;
+    public static int MAX_CHATS_PER_SYNC = 100;
     public static int MAX_MESSAGES_PER_SYNC = 150;
     public static int LISTENER_INTERVAL = 60000 * 5;
     public static int MAX_MESSAGES_PER_CHAT_INITIAL_SYNC = 20;
